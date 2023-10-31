3bb060cb25b419798b75c38dac47f546a727b059
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 17:09:22 +0800

    [PASS] Make the failing test for calculating total price for 3 mangoes pass

--- src/main/java/productcostcalculator/ProductCostCalculator.java
+++ src/main/java/productcostcalculator/ProductCostCalculator.java
@@ -2,7 +2,13 @@ package productcostcalculator;
 
 public class ProductCostCalculator {
   public double compute(String product, int quantity) {
+    int price;
 
-    return 2 * quantity;
+    if("Apple".equals(product)) {
+      price = 2;
+    } else {
+      price = 3;
+    }
+    return price * quantity;
   }
 }
