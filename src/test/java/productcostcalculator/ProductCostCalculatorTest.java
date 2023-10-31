0d56b628c1d49bac59a3dddd20e47bc501cb2afe
Shanyl Jimenez <2245067@cognizant.com>
2023-10-31 14:54:50 +0800

    [Fail] Add failing test for computing total cost of 3 apples.

---
+++ src/test/java/productcostcalculator/ProductCostCalculatorTest.java
@@ -0,0 +1,17 @@
+package productcostcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductCostCalculatorTest {
+
+    @Test
+    void shouldReturn6PesosFor3Apples() {
+        ProductCostCalculator productCostCalculator = new ProductCostCalculator();
+
+        double expectedTotalPrice = productCostCalculator.compute("Apple", 3);
+
+        assertEquals(6, expectedTotalPrice);
+    }
+}
