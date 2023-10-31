5cc1f985d1395c903eac997278c963d18aeb65de
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 18:24:21 +0800

    Notice that this test execute in 10 seconds.
    In the next commit we'll create a mock service instead of actual
    service.

--- src/main/java/com/cognizant/masterclass/tddspringboot/mockito/PriceServiceWithExtensiveComputations.java
+++ src/main/java/com/cognizant/masterclass/tddspringboot/mockito/PriceServiceWithExtensiveComputations.java
@@ -1,2 +1,20 @@
-package com.cognizant.masterclass.tddspringboot.mockito;public class PriceServiceWithExtensiveComputations {
+package com.cognizant.masterclass.tddspringboot.mockito;
+
+import com.cognizant.masterclass.tddspringboot.productcostcalculator.PriceService;
+import com.cognizant.masterclass.tddspringboot.productcostcalculator.SimplePriceService;
+
+public class PriceServiceWithExtensiveComputations implements PriceService {
+
+    private final PriceService priceService = new SimplePriceService();
+
+    @Override
+    public double getPrice(String product) {
+        try {
+            Thread.sleep(10000); // sleep for 10 seconds to simulate extensive computations
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return priceService.getPrice(product);
+    }
 }
--- src/test/java/com/cognizant/masterclass/tddspringboot/mockito/ProductCostCalculatorWithExtensivePriceCalculationTest.java
+++ src/test/java/com/cognizant/masterclass/tddspringboot/mockito/ProductCostCalculatorWithExtensivePriceCalculationTest.java
@@ -1,2 +1,24 @@
-package com.cognizant.masterclass.tddspringboot.mockito;public class ProductCostCalculatorWithExtensivePriceCalculationTest {
+package com.cognizant.masterclass.tddspringboot.mockito;
+
+import com.cognizant.masterclass.tddspringboot.productcostcalculator.Order;
+import com.cognizant.masterclass.tddspringboot.productcostcalculator.ProductCostCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class ProductCostCalculatorWithExtensivePriceCalculationTest {
+
+    @Test
+    void thisTestExecutesIn10Seconds() {
+        ProductCostCalculator productCostCalculator =
+                new ProductCostCalculator(
+                        new PriceServiceWithExtensiveComputations());
+
+        Order order = Order.of("Apple", 3);
+
+        double actualTotalPrice = productCostCalculator.compute(order);
+
+        assertThat(actualTotalPrice, equalTo(6.0));
+    }
 }
