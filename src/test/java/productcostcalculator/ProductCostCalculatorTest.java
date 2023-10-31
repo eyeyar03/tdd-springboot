5f48c5786ba6d2a49e1076b7de5fbfbca1be4d63
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 16:59:55 +0800

    [FAIL] failing test for calculating total price for 3 appleS
    Note, compilation error is a failing test.

--- src/test/java/productcostcalculator/ProductCostCalculatorTest.java
+++ src/test/java/productcostcalculator/ProductCostCalculatorTest.java
@@ -1,2 +1,17 @@
-package productcostcalculator;public class ProductCostCalculatorTest {
+package productcostcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProductCostCalculatorTest {
+
+    @Test
+    void shouldReturn6PesosFor3Apples() {
+        ProductCostCalculator productCostCalculator = new ProductCostCalculator();
+
+        double expectedTotalPrice = productCostCalculator.compute("Apple", 3);
+
+        assertEquals(6, expectedTotalPrice);
+    }
 }
