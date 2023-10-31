6b5af44b22397a6dac70620e3e536835ff7bab6b
Arvin De Los Santos <adsantos.417@gmail.com>
2023-10-31 15:17:29 +0800

    [Pass] Mango fail test

--- src/main/java/com/cognizant/masterclass/tddspringboot/ProductCostCalculator/ProductCostCalculator.java
+++ src/main/java/com/cognizant/masterclass/tddspringboot/ProductCostCalculator/ProductCostCalculator.java
@@ -3,6 +3,15 @@ package com.cognizant.masterclass.tddspringboot.ProductCostCalculator;
 public class ProductCostCalculator {
 
     public Integer compute(String product, int qty) {
-        return  2 * qty;
+
+        if (product.equals("apple")) {
+            return  2 * qty;
+        }
+
+        if (product.equals("mango")) {
+            return 4 * qty;
+        }
+
+        return qty;
     }
 }
