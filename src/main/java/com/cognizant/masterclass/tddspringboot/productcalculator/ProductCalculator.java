ab187aee148499182e349ef295195405d53fb28d
eman027 <eman27pogi@gmail.com>
2023-10-31 15:04:48 +0800

    [Pass] Price of 3 mangos

--- src/main/java/com/cognizant/masterclass/tddspringboot/productcalculator/ProductCalculator.java
+++ src/main/java/com/cognizant/masterclass/tddspringboot/productcalculator/ProductCalculator.java
@@ -1,8 +1,14 @@
 package com.cognizant.masterclass.tddspringboot.productcalculator;
 
+import org.springframework.util.StringUtils;
+
 public class ProductCalculator {
     public int compute(String product, int quantity) {
 
-        return 2 * quantity;
+        if ("Mango".equalsIgnoreCase(product)) {
+            return 3* quantity;
+        } else {
+            return 2 * quantity;
+        }
     }
 }
--- src/test/java/com/cognizant/masterclass/tddspringboot/productcalculator/ProductCalculatorTest.java
+++ src/test/java/com/cognizant/masterclass/tddspringboot/productcalculator/ProductCalculatorTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class ProductCalculatorTest {
 
     @Test
-    public void shouldReturnInvalidPriceFor2Apples() {
+    public void shouldComputePriceFor2Apples() {
         ProductCalculator productCalculator = new ProductCalculator();
-        Assertions.assertEquals(6, productCalculator.compute("Apple", 2));
+        Assertions.assertEquals(4, productCalculator.compute("Apple", 2));
     }
 
     @Test
-    public void shouldRetunInvalidPriceFor3Mangos() {
+    public void shouldComputePriceFor3Mangos() {
         ProductCalculator productCalculator = new ProductCalculator();
         Assertions.assertEquals(9, productCalculator.compute("Mango", 3));
     }
