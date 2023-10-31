480cc3e05f7fea584432053c91eb8558a5f7601f
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 18:31:29 +0800

    Note that in this commit, we already used Mockito to mock PriceService.
    No need to write our own mocked classes.

--- src/test/java/com/cognizant/masterclass/tddspringboot/mockito/ProductCostCalculatorWithExtensivePriceCalculationTest.java
+++ src/test/java/com/cognizant/masterclass/tddspringboot/mockito/ProductCostCalculatorWithExtensivePriceCalculationTest.java
@@ -4,8 +4,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.cognizant.masterclass.tddspringboot.productcostcalculator.Order;
+import com.cognizant.masterclass.tddspringboot.productcostcalculator.PriceService;
 import com.cognizant.masterclass.tddspringboot.productcostcalculator.ProductCostCalculator;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class ProductCostCalculatorWithExtensivePriceCalculationTest {
 
@@ -25,6 +27,21 @@ class ProductCostCalculatorWithExtensivePriceCalculationTest {
   void thisTestWillExecutesFasterUsingMockedPriceService() {
     ProductCostCalculator productCostCalculator =
         new ProductCostCalculator(new MockedPriceService(2.0));
+
+    Order order = Order.of("Apple", 3);
+
+    double actualTotalPrice = productCostCalculator.compute(order);
+
+    assertThat(actualTotalPrice, equalTo(6.0));
+  }
+
+  @Test
+  void thisTestWillExecutesFasterUsingMockito() {
+    PriceService mockedPriceService = Mockito.mock(PriceService.class);
+
+    Mockito.when(mockedPriceService.getPrice("Apple")).thenReturn(2.0);
+
+    ProductCostCalculator productCostCalculator = new ProductCostCalculator(mockedPriceService);
 
     Order order = Order.of("Apple", 3);
 
