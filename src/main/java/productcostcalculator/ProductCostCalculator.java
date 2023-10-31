7442425053b73e061226a2f0f73038eff42d07f6
mariaantoinettef <mariaantoinettefelix@gmail.com>
2023-10-31 15:06:25 +0800

    [Pass] Make failing test for computing total cost for 3 mangoes pass

--- src/main/java/productcostcalculator/ProductCostCalculator.java
+++ src/main/java/productcostcalculator/ProductCostCalculator.java
@@ -3,6 +3,6 @@ package productcostcalculator;
 public class ProductCostCalculator {
 
     public double compute(String product, int quantity) {
-        return 2 * quantity;
+        return 3 * quantity;
     }
 }
