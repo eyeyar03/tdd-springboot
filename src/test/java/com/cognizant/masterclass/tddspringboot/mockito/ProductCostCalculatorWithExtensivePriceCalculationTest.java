ee77e7816e17e4cbd76b0d59cc206f6989855d4e
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 18:27:49 +0800

    The second test, thisTestWillExecutesFasterUsingMockedPriceService, executes faster because we used MockedPriceService.
    We did not call the actual the actual PriceService, but just the mock.
    
    In the next commit, we will use Mockito library instead of writing mock
    classes by hand.

--- src/test/java/com/cognizant/masterclass/tddspringboot/mockito/ProductCostCalculatorWithExtensivePriceCalculationTest.java
+++ src/test/java/com/cognizant/masterclass/tddspringboot/mockito/ProductCostCalculatorWithExtensivePriceCalculationTest.java
@@ -1,24 +1,35 @@
 package com.cognizant.masterclass.tddspringboot.mockito;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import com.cognizant.masterclass.tddspringboot.productcostcalculator.Order;
 import com.cognizant.masterclass.tddspringboot.productcostcalculator.ProductCostCalculator;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
 class ProductCostCalculatorWithExtensivePriceCalculationTest {
 
-    @Test
-    void thisTestExecutesIn10Seconds() {
-        ProductCostCalculator productCostCalculator =
-                new ProductCostCalculator(
-                        new PriceServiceWithExtensiveComputations());
+  @Test
+  void thisTestExecutesIn10Seconds() {
+    ProductCostCalculator productCostCalculator =
+        new ProductCostCalculator(new PriceServiceWithExtensiveComputations());
 
-        Order order = Order.of("Apple", 3);
+    Order order = Order.of("Apple", 3);
 
-        double actualTotalPrice = productCostCalculator.compute(order);
+    double actualTotalPrice = productCostCalculator.compute(order);
 
-        assertThat(actualTotalPrice, equalTo(6.0));
-    }
+    assertThat(actualTotalPrice, equalTo(6.0));
+  }
+
+  @Test
+  void thisTestWillExecutesFasterUsingMockedPriceService() {
+    ProductCostCalculator productCostCalculator =
+        new ProductCostCalculator(new MockedPriceService(2.0));
+
+    Order order = Order.of("Apple", 3);
+
+    double actualTotalPrice = productCostCalculator.compute(order);
+
+    assertThat(actualTotalPrice, equalTo(6.0));
+  }
 }
