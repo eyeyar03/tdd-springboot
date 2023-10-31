d645a9aa4c30d9436d21e91c7113279f163bcda1
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 17:12:37 +0800

    [FAIL] Add failing test for price not found

--- src/test/java/productcostcalculator/ProductCostCalculatorTest.java
+++ src/test/java/productcostcalculator/ProductCostCalculatorTest.java
@@ -1,26 +1,34 @@
 package productcostcalculator;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class ProductCostCalculatorTest {
 
-    @Test
-    void shouldReturn6PesosFor3Apples() {
-        ProductCostCalculator productCostCalculator = new ProductCostCalculator();
+  @Test
+  void shouldReturn6PesosFor3Apples() {
+    ProductCostCalculator productCostCalculator = new ProductCostCalculator();
 
-        double expectedTotalPrice = productCostCalculator.compute("Apple", 3);
+    double expectedTotalPrice = productCostCalculator.compute("Apple", 3);
 
-        assertEquals(6, expectedTotalPrice);
-    }
+    assertEquals(6, expectedTotalPrice);
+  }
 
-    @Test
-    void shouldReturn9PesosFor3Mangoes() {
-        ProductCostCalculator productCostCalculator = new ProductCostCalculator();
+  @Test
+  void shouldReturn9PesosFor3Mangoes() {
+    ProductCostCalculator productCostCalculator = new ProductCostCalculator();
 
-        double expectedTotalPrice = productCostCalculator.compute("Mango", 3);
+    double expectedTotalPrice = productCostCalculator.compute("Mango", 3);
 
-        assertEquals(9, expectedTotalPrice);
-    }
+    assertEquals(9, expectedTotalPrice);
+  }
+
+  @Test
+  void shouldThrowPriceNotFoundExceptionWhenNoCorrespondingPriceFound() {
+    ProductCostCalculator productCostCalculator = new ProductCostCalculator();
+
+    assertThrows(PriceNotFoundException.class, () -> productCostCalculator.compute("Tomato", 3));
+  }
 }
