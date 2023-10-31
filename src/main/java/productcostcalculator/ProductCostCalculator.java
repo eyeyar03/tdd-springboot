5546bda18ad1635a2ecd23548981955f9c1b621b
Aaron Macandili <aaron.macandili@cognizant.com>
2023-10-31 17:02:59 +0800

    [PASS] Make the failing test for calculating total price for 3 apples

--- src/main/java/productcostcalculator/ProductCostCalculator.java
+++ src/main/java/productcostcalculator/ProductCostCalculator.java
@@ -1,2 +1,8 @@
-package productcostcalculator;public class ProductCostCalculator {
+package productcostcalculator;
+
+public class ProductCostCalculator {
+  public double compute(String product, int quantity) {
+
+    return 2 * quantity;
+  }
 }
