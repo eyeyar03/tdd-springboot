4274994c83a64b604a60fdcfc29e468f844d5486
junbugtong <39414968+junbugtong@users.noreply.github.com>
2023-11-05 21:38:05 +0800

    Update ProductCostCalculator.java
    [Pass] Make the failing test for computing total cost for 2 apples pass.

--- src/main/java/productcostcalculator/ProductCostCalculator.java
+++ src/main/java/productcostcalculator/ProductCostCalculator.java
@@ -2,6 +2,6 @@ package productcostcalculator;
 
 public class ProductCostCalculator {
     public double compute(String product, int quantity) {
-        return 0.0;
+        return 2 * quantity;
     }
 }
