a57da8a725459a4fd69eab80a90cc7b7c25a8805
junbugtong <39414968+junbugtong@users.noreply.github.com>
2023-11-05 21:33:07 +0800

    Create ProductCostCalculatorTest.java
    [Fail] Add failing test for computing total cost for 3 apples

---
+++ src/test/java/productcostcalculator/ProductCostCalculatorTest.java
@@ -0,0 +1,18 @@
+package productcostcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProductCostCalculatorTest {
+
+    @Test
+    void shouldReturnComputedPriceFor3Apples() {
+        ProductCostCalculator productCostCalculator = new ProductCostCalculator();
+
+        double expectedTotalPrice = productCostCalculator.compute("Apple", 3);
+
+        assertEquals(6, expectedTotalPrice);
+    }
+
+}
